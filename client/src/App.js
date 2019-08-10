b9981fa59b6cc99b090505db50601c352818205a
Emmanuel Tanifum <etantanifum@gmail.com>
2019-08-10 13:32:00 -0400

    got data from API rendered on screen with some more notes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
-import FormikMyForm from './components/Form'
+import FormikMyForm from './components/Form';
 
-function App() {
-  return (
-    <div className="App">
-      <FormikMyForm />
-    </div>
-  );
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        {/* Importing the form onto the main component app */}
+        <FormikMyForm />
+      </div>
+    );
+  }
 }
-
 export default App;
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -6,11 +6,11 @@ import axios from 'axios';
 //objects with key value pairs down below...
 const MyForm = ({ touched,errors,status,isSubmitting,values}) => {
 
-  const [users, setUsers] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
     if (status) {
-      setUsers([...users, status]);
+      setRecipes([...status]);
     }
   }, [status]);
 
@@ -32,11 +32,22 @@ const MyForm = ({ touched,errors,status,isSubmitting,values}) => {
            <button disabled={isSubmitting} type='submit'>Submit</button> 
 
            <label className="checkbox-container">
-                    Accept confidentiality agreement?
+                    Accept User Manual?
             <Field type="checkbox" name="tos" checked={values.tos} />
              <span className="checkmark" />
             </label>
         </Form>
+
+    {/* mapping over array of function to recive data back from recipe */}
+        <p>RECIPES:</p>{
+            recipes ? recipes.map(recipe => (
+
+                <p key={Date.now() + Math.random(10000)}> Name : {recipe.name} Course : {recipe.course}  </p>
+              
+              )
+              )
+            : null}
+        
         
         
       </div>
@@ -56,30 +67,41 @@ const FormikMyForm = withFormik({
       };
     },
   //Yup validation Schema is used for data validation and errors are mapped to iner component errors meaning its keys should match those of values
-    validationSchema: Yup.object().shape({
-      username: Yup.string() .required('Username is required').min(6, 'Username must be at least 5 characters long.'),
-      password: Yup.string().min(6, 'Password must be at least 6 characters long.').required('Password is required'),
+    validationSchema: Yup.object().shape({username:Yup.string().required('Username is required').min(6, 'Username must be at least 5 characters long.'),password:Yup.string().min(6, 'Password must be at least 6 characters long.').required('Password is required'),
     }),
-  
-    handleSubmit(values, { setStatus, setErrors, resetForm }) {
+    handleSubmit(values, { setSubmitting, setStatus, setErrors, resetForm }) {
     //when the values get submitted then we send the data to the API and the form resets itself
 
       if (values.username === "Mannyyy") {
         setErrors({ username: "That username is already taken by someone else" });
       } else {
+
         // saving data to the server using axios
         axios
-          .post(`http://localhost:5000/api/register`, values)
-          .then(response => {
-            console.log('res in axios', response.data)
-            setStatus(response.data);
-            localStorage.setItem('', response.data.token);
-            resetForm();
-          })
-          .catch(error =>
-            console.log("Error in handleSubmit post call", error.response)
-          );
-        
+        .post("http://localhost:5000/api/register", values)
+        .then(res => {
+          resetForm();
+          setStatus(res.data);
+          console.log(res.data);
+          setSubmitting(false);
+        })
+        .catch(err => {
+          console.log(err);
+          setSubmitting(false);
+        })
+
+
+        //recieving list of recipes from server using the .get axios method?
+        axios
+      .get("http://localhost:5000/api/restricted/data")
+      .then(response => {
+        setStatus(response.data);
+        console.log(response.data);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+           
       }
     }
   })(MyForm);
