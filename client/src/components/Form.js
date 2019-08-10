3c2a1f8aeac6bc4cd27f7e72495b1dc07e5613cf
Emmanuel Tanifum <etantanifum@gmail.com>
2019-08-10 10:14:37 -0400

    took down some personal notes as to what is going on and added a checkbox feild

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -3,8 +3,8 @@ import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 
-
-const MyForm = ({ touched,errors,status,values }) => {
+//objects with key value pairs down below...
+const MyForm = ({ touched,errors,status,isSubmitting,values}) => {
 
   const [users, setUsers] = useState([]);
 
@@ -19,13 +19,23 @@ const MyForm = ({ touched,errors,status,values }) => {
         <h1>Registration</h1>
         <Form>
           <Field type='text' name='username' placeholder='username' className='form-field' />
+          
+        {/* if errors exist then username error will show up that is if username was touched */}
+
           {touched.username && errors.username && (<p className='error'>{errors.username}</p> )}
           
-          
           <Field type='password'  name='password' placeholder='Password' className='form-field' />
-          {touched.password && errors.password && ( <p className='error'>{errors.password}</p>  )}
-           <button type='submit'>Submit</button> 
+          
+          {/* if errors exist then password error will show up that is if password was touched */}
 
+          {touched.password && errors.password && ( <p className='error'>{errors.password}</p>  )}
+           <button disabled={isSubmitting} type='submit'>Submit</button> 
+
+           <label className="checkbox-container">
+                    Accept confidentiality agreement?
+            <Field type="checkbox" name="tos" checked={values.tos} />
+             <span className="checkmark" />
+            </label>
         </Form>
         
         
@@ -33,31 +43,31 @@ const MyForm = ({ touched,errors,status,values }) => {
     );
   
 };
+
+//Higher order component-->passing in what to do when the form gets submitted
 const FormikMyForm = withFormik({
+
+//returns objects , key value pairs
+
     mapPropsToValues({ username, password }) {
       return {
         username: username || '',
         password: password || ''
       };
     },
-  
+  //Yup validation Schema is used for data validation and errors are mapped to iner component errors meaning its keys should match those of values
     validationSchema: Yup.object().shape({
-      username: Yup
-        .string()
-        .required('You have to provide a username')
-        .min(6, 'Username must be at least 5 characters long.'),
-      password: Yup
-        .string()
-        .min(6, 'Password must be at least 6 characters long.')
-        .required('You need a password to log in'),
+      username: Yup.string() .required('Username is required').min(6, 'Username must be at least 5 characters long.'),
+      password: Yup.string().min(6, 'Password must be at least 6 characters long.').required('Password is required'),
     }),
   
     handleSubmit(values, { setStatus, setErrors, resetForm }) {
-    
+    //when the values get submitted then we send the data to the API and the form resets itself
+
       if (values.username === "Mannyyy") {
         setErrors({ username: "That username is already taken by someone else" });
       } else {
-  
+        // saving data to the server using axios
         axios
           .post(`http://localhost:5000/api/register`, values)
           .then(response => {
